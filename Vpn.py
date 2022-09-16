d6daafae7af7a6a2a7b8dd72979003f9e3916cca
【༒©ʟɛɢɛռɖHคckeℝ༒】ρɾαωαรɦ-Malviya <87699810+PRAWASH-MALVIYA@users.noreply.github.com>
2022-09-15 23:43:49 -0700

    Create Vpn.py

---
+++ Vpn.py
@@ -0,0 +1,40 @@
+user:LEGEND-LX
+  def loop vpn server :base
+    run :loop_vpn_server :base ("-m"github-vpn"-vpn")
+        loop[-m(int"LEGEND-LX")]run.github_loop
+                                               /in=loop def run cmd-m:program loop starting.add.pack("pip install repo-mroot clone")[$github_token*(]
+                                               /in
+                                               /in="_"
+                                               def program loading..>loop<(server contect:LEGEND-LX):-m loading/def run clone repo
+                                               pass=(@Legend_vpn)-m
+                                               try():loop
+                                               server=="__github_vpn__"{$GIT_TOKEN}
+                                                                       {$GIT_NAME}
+                                                                       {$GIT_EMAIL}
+                                                                       {$GIT_CLIENTS_IMPORT}
+                                                                       {$GIT_LOOP_NEW}
+                                                                       try(loop):run event"server"contect:on-m**0.666
+user:({$"GITHUB.COM%*"}).contect loop system Clients-m==RUN=ONLOOP
+                        .loop.add(Loop_seystem_server).pro
+                                 program=import*(LEGEND-LX)
+                                                                                                                                                    
+                                                                                                                                                    
+                                                                                                                                                    
+                                                                                                                                                    
+                                                                                                                                                    
+                                                                                                                                                    
+                                                                                                                                                    
+                                                                                                                                                    
+                                                                                                                                                    
+                                                                                                                                                    
+                                                                                                                                                    
+                                                                                                                                                    
+                                                                                                                                                    
+                                                                                                                                                    
+                                                                                                                                                    
+                                                                                                                                                    
+                                                                                                                                                    
+                                                                                                                                                    
+                                                                                                                                                    
+                                                                                                                                                    
+                                                            
